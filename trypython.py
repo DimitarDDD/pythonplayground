8d18b1762c85a467509878e546ed1a89a7297ab4
DimitarD <dimitardobrikov77@gmail.com>
2018-07-09 12:32:47 +0000

    latest challanges

---
+++ trypython.py
@@ -0,0 +1,24 @@
+# print ('Hello World') 
+# print ('Welcometo Python')
+# name = raw_input('What is your name?') 
+# print ('Hello ' + name)
+
+
+#num1 = int(input('enter first number: '))
+#num2 = int(input('enter second number: ')) 
+#sum = num1 + num2
+
+#print(sum)
+ 
+#s= 'Hello' 
+#str=""
+#for i in s: 
+    #str = i + str 
+    #print(str) 
+ 
+num1 = int(input('enter number here : '))     
+if num1 <=10:
+    print ("small")
+else:
+    print ("Big")
+     
